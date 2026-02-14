1f14255e40f19dd53f723bff3f08a3f28ccbe51f
KhalilSailan <khaleelsailan@email.com>
2026-02-15 00:50:02 +0300

    modify product resource

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Nette\Utils\Random;
 
 class ProductResource extends JsonResource
 {
@@ -15,11 +16,16 @@ class ProductResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'name'=>$this->name,
-            'descruption'=>$this->detail,
-            'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount,
+            'name' => $this->name,
+            'descruption' => $this->detail,
+            'price' => $this->price,
+            'stock' => $this->stock == 0 ? "out the stock " : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount / 100)) * $this->price, 2),
+            'ranig' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
     }
 }
